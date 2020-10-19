d08dd2e30320aaf56d4cad78f5fc7b2a5ae65abd
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2020-10-19 13:27:38 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,33 +7,6 @@ document.getElementById('c').style.backgroundImage= "url('star.jpg')";
 document.getElementById('c').style.backgroundRepeat= "no-repeat";
 document.getElementById('c').style.backgroundPosition="center";
 
-
-
-
-function function2(){
-
-
- var formc=document.getElementById('c').style.background = "#7ccd7c";
-
-}
-
-
-function function3(){
-
-
-/* var formc=document.getElementById('c').style.background = "#e6e6fa";*/
-
-var formc=document.getElementById('c').style.backgroundImage= "url('star.jpg')";
-var formca=document.getElementById('c').style.backgroundRepeat= "no-repeat";
-var formcb=document.getElementById('c').style.backgroundPosition="center";
-
-
-
-
-}
-
-
-
 function function1() {
   var x = document.getElementById('f');
   if (x.style.display === 'none') {
@@ -41,7 +14,28 @@ function function1() {
   } else {
     x.style.display = 'none';
   }
+  }
+
+function function2(){
+var formc=document.getElementById('c').style.background = "#7ccd7c";
+
 }
+
+
+function function3(){
+/* var formc=document.getElementById('c').style.background = "#e6e6fa";*/
+
+var formc=document.getElementById('c').style.backgroundImage= "url('star.jpg')";
+var formca=document.getElementById('c').style.backgroundRepeat= "no-repeat";
+var formcb=document.getElementById('c').style.backgroundPosition="center";
+
+
+}
+
+
+
+
+
 
 
 
